d961b716b57d3b5a0aeefb1cc822de54617dc245
tienthinh-fe <thinhhtde150233@fpt.edu.vn>
2023-06-04 22:16:47 +0700

    fix: disable user select at step

--- src/components/organisms/ListStep/ListStep.tsx
+++ src/components/organisms/ListStep/ListStep.tsx
@@ -7,6 +7,7 @@ import styled from 'styled-components'
 const Container = styled.div`
   height: 568px;
   background-image: url(/background.svg);
+  user-select: none;
 `
 
 const ListWrapper = styled.div`
